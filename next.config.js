cdfe843bd1081d7f333f2d2f2999e9fcdade7e6b
Yan <picturepan2@hotmail.com>
2023-04-27 01:56:57 +0800

    Cleanup and Redirects

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => [
+    {
+      has: [
+        {
+          type: "host",
+          value: "api.web3.bio",
+        },
+      ],
+      source: "/api/:path*",
+      destination: "/:path*",
+      permanent: true,
+    },
+  ],
 }
 
 module.exports = nextConfig
