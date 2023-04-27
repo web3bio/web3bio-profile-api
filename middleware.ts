a602a1ef556ca74a86eaf671facba8f12df1e941
billy <Billy@mask.io>
2023-04-27 16:12:38 +0800

    feat: middleware

---
+++ middleware.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from "next/server";
+
+const Middleware = (req: { nextUrl: { pathname: string; origin: string } }) => {
+  if (req.nextUrl.pathname === req.nextUrl.pathname.toLowerCase()) {
+    return NextResponse.next();
+  }
+  return NextResponse.redirect(
+    new URL(req.nextUrl.origin + req.nextUrl.pathname.toLowerCase())
+  );
+};
+
+export default Middleware;
