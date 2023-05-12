4568e70d778a5604cafcfd1a092d6ad907db1f80
Yan <picturepan2@hotmail.com>
2023-05-12 10:45:12 +0800

    Add experimental Edge

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: false,
+  experimental: { 
+    runtime: "edge"
+  },
   redirects: async () => [
     {
       source: "/api/:path*",
