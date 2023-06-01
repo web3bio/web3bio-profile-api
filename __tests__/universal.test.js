9a4db5bcef29470dd0d30448fc38578534983c07
billy <Billy@mask.io>
2023-06-01 14:06:13 +0800

    chore: add some test case

--- __tests__/universal.test.js
+++ __tests__/universal.test.js
@@ -13,9 +13,35 @@ describe("Test For Universal Profile API", () => {
   }, 200000);
   it("It should response empty data for mcdonalds.eth", async () => {
     const res = await queryClient("/profile/mcdonalds.eth");
-    expect(res.status).toBe(200);
+    expect(res.status).toBe(404);
     const json = await res.json();
     expect(json.total).toBe(0);
-    expect(json.results).toBe([]);
+  }, 50000);
+  it("It should response 200 data for stani.lens", async () => {
+    const res = await queryClient("/profile/stani.lens");
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    const json2 = await (
+      await queryClient(
+        `/profile/${json.results.find((x) => x.platform === "lens")?.address}`
+      )
+    ).json();
+    console.log(json, json2, "response");
+    expect(json.total).toBe(json2.total);
+  }, 200000);
+  it("It should response 200 data for brantly.eth", async () => {
+    const res = await queryClient("/profile/brantly.eth");
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    const linksOBJ = json.results.find((x) => x.platform === "ENS").links;
+    const links = Object.keys(linksOBJ);
+    const isValidHandle = (() => {
+      for (let i in linksOBJ) {
+        if (linksOBJ[i]?.handle.includes("@")) return false;
+        return true;
+      }
+    })();
+    expect(links.length).toBe(6);
+    expect(isValidHandle).toBe(true);
   }, 200000);
 });
