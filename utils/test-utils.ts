4c3d31d670c26d6df2c807951ecf737818fa6e09
Yan <picturepan2@hotmail.com>
2023-06-09 12:02:43 +0800

    Update test-utils.ts

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -1,6 +1,6 @@
 import fetch from "node-fetch";
 
-const baseURL = "http://localhost:3000/api" || process.env.VERCEL_URL;
+const baseURL = process.env.VERCEL_URL || "http://localhost:3000/api";
 // const baseURL = 'https://api.web3.bio'
 
 export const queryClient = async (path: string) => {
