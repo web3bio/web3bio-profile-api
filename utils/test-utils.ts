a3f0d69bc17a1ed3be2cad5bb7141cb22efd5a91
Yan <picturepan2@hotmail.com>
2023-06-09 16:43:01 +0800

    Update NEXT_PUBLIC_VERCEL_URL

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -1,8 +1,8 @@
 import fetch from "node-fetch";
 
-const { VERCEL_URL } = process.env;
+const { NEXT_PUBLIC_VERCEL_URL } = process.env;
 
-const baseURL = `https://${VERCEL_URL}` || "http://localhost:3000";
+const baseURL = `https://${NEXT_PUBLIC_VERCEL_URL}` || "http://localhost:3000";
 // const baseURL = 'https://api.web3.bio'
 
 export const queryClient = async (path: string) => {
