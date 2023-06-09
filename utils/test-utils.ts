416594ca2508797439bab4b9f85fa122836130ad
billy <Billy@mask.io>
2023-06-09 16:21:00 +0800

    style: protocol

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 
 const { VERCEL_URL } = process.env;
 
-const baseURL = VERCEL_URL || "http://localhost:3000";
+const baseURL = `https://${VERCEL_URL}` || "http://localhost:3000";
 // const baseURL = 'https://api.web3.bio'
 
 export const queryClient = async (path: string) => {
