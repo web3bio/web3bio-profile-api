1f851a66b0c0848915781ccb1b4b826293d98f70
Yan <picturepan2@hotmail.com>
2023-09-15 02:35:14 +0800

    Update cache policy

--- utils/base.ts
+++ utils/base.ts
@@ -75,7 +75,7 @@ export const respondWithCache = (json: string) => {
     status: 200,
     headers: {
       "Cache-Control": `public, s-maxage=${
-        60 * 60 * 24 * 7
+        60 * 60 * 24
       }, stale-while-revalidate=${60 * 30}`,
     },
   });
