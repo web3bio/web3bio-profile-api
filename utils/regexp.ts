e4fd2c3ad628e9e1f3110d835c58aba44e69ea97
Yan <picturepan2@hotmail.com>
2023-11-13 23:59:16 +0800

    Update regex

--- utils/regexp.ts
+++ utils/regexp.ts
@@ -3,7 +3,7 @@ export const regexEns = /.*\.(eth|xyz|app|luxe|kred|art|ceo|club)$/i,
   regexDotbit = /.*\.bit$/i,
   regexEth = /^0x[a-fA-F0-9]{40}$/i,
   regexTwitter = /^[A-Za-z0-9_]{1,15}$/i,
-  regexFarcaster = /^[A-Za-z0-9_.-]{1,61}($|\.farcaster$)/i,
+  regexFarcaster = /^[A-Za-z0-9_-]{1,61}(?:|\.eth)(?:|\.farcaster)$/i,
   regexUnstoppableDomains =
     /.*\.(crypto|888|nft|blockchain|bitcoin|dao|x|klever|hi|zil|kresus|polygon|wallet|binanceus|anime|go|manga|eth)$/i,
   regexSpaceid = /.*\.(bnb|arb)$/i,
