64a2f3697581dfb1e900e13fad931cb2ecdfff2b
billy <Billy@mask.io>
2023-12-13 14:40:46 +0800

    chore: init refact

--- pages/api/profile/[handle].ts
+++ pages/api/profile/[handle].ts
@@ -57,8 +57,8 @@ const sortByPlatform = (
 ) => {
   const defaultOrder = [
     PlatformType.ens,
-    PlatformType.lens,
     PlatformType.farcaster,
+    PlatformType.lens,
     PlatformType.dotbit,
     PlatformType.unstoppableDomains,
   ];
@@ -168,6 +168,12 @@ const resolveUniversalRespondFromRelation = async ({
             (response as PromiseFulfilledResult<ProfileAPIResponse>)?.value
         );
       const returnRes = sortByPlatform(responsesToSort, platform, handle);
+      if (
+        platform === PlatformType.ethereum &&
+        !returnRes.some((x) => x?.identity === handle)
+      ) {
+        returnRes.unshift(responsesToSort.find((x) => x?.identity === handle));
+      }
       if (!returnRes?.length && platform === PlatformType.ethereum) {
         const nsObj = {
           address: handle,
