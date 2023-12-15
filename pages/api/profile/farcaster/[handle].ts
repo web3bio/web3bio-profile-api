63604122e2f57fa8e610c219d069c4758b4f776d
billy <Billy@mask.io>
2023-12-15 12:59:51 +0800

    chore: add .farcaster query support in farcaster profile query

--- pages/api/profile/farcaster/[handle].ts
+++ pages/api/profile/farcaster/[handle].ts
@@ -153,14 +153,22 @@ export default async function handler(req: NextApiRequest) {
   const inputName = searchParams.get("handle");
   const lowercaseName = inputName?.toLowerCase() || "";
 
-  if (!regexFarcaster.test(lowercaseName) && !regexEth.test(lowercaseName))
+  if (
+    !regexFarcaster.test(lowercaseName) &&
+    !regexEth.test(lowercaseName) &&
+    !lowercaseName.endsWith(".farcaster")
+  )
     return errorHandle({
       identity: lowercaseName,
       platform: PlatformType.farcaster,
       code: 404,
       message: ErrorMessages.invalidIdentity,
     });
-  return resolveFarcasterRespond(lowercaseName);
+  const queryInput = lowercaseName.endsWith(".farcaster")
+    ? lowercaseName.replace(".farcaster", "")
+    : lowercaseName;
+    
+  return resolveFarcasterRespond(queryInput);
 }
 
 export const config = {
