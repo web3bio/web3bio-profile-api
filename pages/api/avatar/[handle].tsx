81d849d5ad87619ba4f9035689e1f5b361b8095b
billy <Billy@mask.io>
2024-04-24 16:25:20 +0800

    chore: feat avatar api with regex platform

--- pages/api/avatar/[handle].tsx
+++ pages/api/avatar/[handle].tsx
@@ -2,31 +2,58 @@ import React from "react";
 import ReactDOMServer from "react-dom/server";
 import type { NextApiRequest } from "next";
 import Avatar, { AvatarProps } from "boring-avatars";
+import {
+  baseURL,
+  handleSearchPlatform,
+  shouldPlatformFetch,
+} from "@/utils/base";
+import Image from "next/image";
 
 // Demo: http://localhost:3000/avatar/vitalik.eth
 
 export default async function handler(req: NextApiRequest) {
   const searchParams = new URLSearchParams(req.url?.split("?")[1] || "");
+  let avatarHTML = "";
   const name = searchParams.get("handle") || "";
   const size = searchParams.get("size") || 160;
-  const variant = searchParams.get("variant") || "bauhaus";
-  const colors = ["#4b538b", "#15191d", "#f7a21b", "#e45635", "#d60257"];
-  
-  const avatarHtml = ReactDOMServer.renderToString(
-    <Avatar
-      {...{
-        name,
-        size,
-        variant: variant as AvatarProps["variant"],
-        colors,
-      }}
-    />
-  );
-  return new Response(avatarHtml, {
+  const platform = handleSearchPlatform(name);
+  if (shouldPlatformFetch(platform)) {
+    const profile = await fetch(baseURL + `/profile/${platform}/${name}`).then(
+      (res) => res.json()
+    );
+    if (!profile.avatar) return;
+    avatarHTML = ReactDOMServer.renderToString(
+      <Image
+        style={{
+          borderRadius: 99
+        }}
+        width={Number(size)}
+        height={Number(size)}
+        alt={profile.identity}
+        src={profile.avatar}
+      />
+    );
+  } else {
+    const variant = searchParams.get("variant") || "bauhaus";
+    const colors = ["#4b538b", "#15191d", "#f7a21b", "#e45635", "#d60257"];
+
+    avatarHTML = ReactDOMServer.renderToString(
+      <Avatar
+        {...{
+          name,
+          size,
+          variant: variant as AvatarProps["variant"],
+          colors,
+        }}
+      />
+    );
+  }
+
+  return new Response(avatarHTML, {
     status: 200,
     headers: {
       "Cache-Control": "public, s-maxage=86400, stale-while-revalidate=3600",
-      "Content-Type": "image/svg+xml",
+      "Content-Type": "image/svg+xml, text/html",
     },
   });
 }
--- utils/base.ts
+++ utils/base.ts
@@ -13,6 +13,12 @@ import {
 } from "./regexp";
 import { errorHandleProps } from "./types";
 
+const { NEXT_PUBLIC_VERCEL_URL } = process.env;
+
+export const baseURL = NEXT_PUBLIC_VERCEL_URL
+  ? `https://${NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
+
 export const errorHandle = (props: errorHandleProps) => {
   const isValidAddress = isValidEthereumAddress(props.identity || "");
   return new Response(
