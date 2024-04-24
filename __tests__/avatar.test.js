2d2214e7eddec147147772f499bd5cf397e4dad7
billy <Billy@mask.io>
2024-04-24 16:48:44 +0800

    chore: uppercase

--- __tests__/avatar.test.js
+++ __tests__/avatar.test.js
@@ -19,4 +19,12 @@ describe("Test For Avatar Service API", () => {
     const text = await res.text();
     expect(text.startsWith("<svg"));
   });
+  it("It should response 200 for 46YaTaa8Xa1xFEVDxPa4CVJpzsNADocgixS51HLNCS4Y", async () => {
+    const res = await queryClient(
+      "/avatar/46YaTaa8Xa1xFEVDxPa4CVJpzsNADocgixS51HLNCS4Y"
+    );
+    expect(res.status).toBe(200);
+    const text = await res.text();
+    expect(text.startsWith("<img"));
+  });
 });
