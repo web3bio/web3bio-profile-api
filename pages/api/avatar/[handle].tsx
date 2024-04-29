12480fe04e3127a2c1b124fdeae152e1e145aaac
billy <Billy@mask.io>
2024-04-29 12:04:23 +0800

    chore: some

--- pages/api/avatar/[handle].tsx
+++ pages/api/avatar/[handle].tsx
@@ -4,6 +4,7 @@ import type { NextApiRequest } from "next";
 import Avatar, { AvatarProps } from "boring-avatars";
 import {
   baseURL,
+  errorHandle,
   handleSearchPlatform,
   shouldPlatformFetch,
 } from "@/utils/base";
@@ -14,20 +15,29 @@ export default async function handler(req: NextApiRequest) {
   const name = searchParams.get("handle") || "";
   const size = searchParams.get("size") || 160;
   const platform = handleSearchPlatform(name);
-  if (shouldPlatformFetch(platform)) {
-    const profiles = (await fetch(baseURL + `/ns/${name}`)
-      .then((res) => res.json())
-      .catch((e) => null)) as any;
-    if (profiles?.length > 0) {
-      const avatarURL = profiles?.find(
-        (x: { avatar: string | null }) => x.avatar !== null
-      )?.avatar;
-      if (avatarURL) {
-        return respondWithCache(avatarURL, {
-          "Content-Type": "application/json",
-        });
+  try {
+    if (shouldPlatformFetch(platform)) {
+      const profiles = (await fetch(baseURL + `/ns/${name}`)
+        .then((res) => res.json())
+        .catch((e) => null)) as any;
+      if (profiles?.length > 0) {
+        const avatarURL = profiles?.find(
+          (x: { avatar: string | null }) => x.avatar !== null
+        )?.avatar;
+        if (avatarURL) {
+          return respondWithCache(avatarURL, {
+            "Content-Type": "application/json",
+          });
+        }
       }
     }
+  } catch (e: any) {
+    return errorHandle({
+      code: 404,
+      platform: null,
+      identity: null,
+      message: e.message as string,
+    });
   }
 
   // const variant = searchParams.get("variant") || "bauhaus";
