560d26f780429af412ba99b4a6ed1956a41f1fe1
billy <Billy@mask.io>
2024-05-13 17:36:55 +0800

    chore: response body && headers

--- app/api/avatar/[handle]/route.ts
+++ app/api/avatar/[handle]/route.ts
@@ -3,7 +3,7 @@ import {
   handleSearchPlatform,
   shouldPlatformFetch,
 } from "@/utils/base";
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest } from "next/server";
 import { resolveUniversalRespondFromRelation } from "../../profile/[handle]/utils";
 import { respondWithSVG } from "../svg/utils";
 
@@ -11,36 +11,38 @@ export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const name = searchParams.get("handle") || "";
   const platform = handleSearchPlatform(name);
-  if (shouldPlatformFetch(platform)) {
-    const profiles = (await resolveUniversalRespondFromRelation({
-      platform,
-      handle: name,
-      req,
-      ns: true,
-    })) as any;
+  try {
+    if (shouldPlatformFetch(platform)) {
+      const profiles = (await resolveUniversalRespondFromRelation({
+        platform,
+        handle: name,
+        req,
+        ns: true,
+      })) as any;
 
-    if (profiles?.length > 0) {
-      const rawAvatarUrl = profiles?.find((x: any) => !!x.avatar)?.avatar;
-      let avatarURL = rawAvatarUrl;
-      if (avatarURL?.includes(".webp")) {
-        avatarURL = `${baseURL}/avatar/process?url=${encodeURIComponent(
-          rawAvatarUrl
-        )}`;
-        const arrayBuffer = await fetch(avatarURL)
-          .then((res) => res.arrayBuffer())
-          .catch(() => null);
-        if (arrayBuffer) {
-          return new Response(arrayBuffer, {
+      if (profiles?.length > 0) {
+        const rawAvatarUrl = profiles?.find((x: any) => !!x.avatar)?.avatar;
+        let avatarURL = rawAvatarUrl;
+        if (avatarURL?.includes(".webp")) {
+          avatarURL = `${baseURL}/avatar/process?url=${encodeURIComponent(
+            rawAvatarUrl
+          )}`;
+        }
+        const response = await fetch(avatarURL);
+        if (response) {
+          return new Response(response.body, {
             headers: {
+              "Content-Type":
+                response.headers.get("content-type") || "image/png",
               "Cache-Control":
                 "public, s-maxage=604800, stale-while-revalidate=86400",
             },
           });
         }
-      } else {
-        return NextResponse.redirect(avatarURL);
       }
     }
+  } catch (e) {
+    return respondWithSVG(name, 240);
   }
   return respondWithSVG(name, 240);
 }
