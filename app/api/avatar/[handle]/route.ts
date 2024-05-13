fe72312b43836570974c5753ce6ae9a38ad1bcfb
billy <Billy@mask.io>
2024-05-13 18:18:05 +0800

    chore: test

--- app/api/avatar/[handle]/route.ts
+++ app/api/avatar/[handle]/route.ts
@@ -12,39 +12,36 @@ export async function GET(req: NextRequest) {
   const name = searchParams.get("handle") || "";
   const platform = handleSearchPlatform(name);
   let avatarURL = "";
-  try {
-    if (shouldPlatformFetch(platform)) {
-      const profiles = (await resolveUniversalRespondFromRelation({
-        platform,
-        handle: name,
-        req,
-        ns: true,
-      })) as any;
+  if (shouldPlatformFetch(platform)) {
+    const profiles = (await resolveUniversalRespondFromRelation({
+      platform,
+      handle: name,
+      req,
+      ns: true,
+    })) as any;
 
-      if (profiles?.length > 0) {
-        const rawAvatarUrl = profiles?.find((x: any) => !!x.avatar)?.avatar;
-        avatarURL = rawAvatarUrl;
-        if (rawAvatarUrl?.includes(".webp")) {
-          avatarURL = `${baseURL}/avatar/process?url=${encodeURIComponent(
-            rawAvatarUrl
-          )}`;
-        }
-        const response = await fetch(avatarURL, {
-          redirect: "error",
-        }).then((res) => res.arrayBuffer());
-        if (response) {
-          return new Response(response, {
-            headers: {
-              "Cache-Control":
-                "public, s-maxage=604800, stale-while-revalidate=86400",
-            },
-          });
-        }
+    if (profiles?.length > 0) {
+      const rawAvatarUrl = profiles?.find((x: any) => !!x.avatar)?.avatar;
+      avatarURL = rawAvatarUrl;
+      if (rawAvatarUrl?.includes(".webp")) {
+        avatarURL = `${baseURL}/avatar/process?url=${encodeURIComponent(
+          rawAvatarUrl
+        )}`;
+      }
+      const response = await fetch(avatarURL, {
+        redirect: "manual",
+      });
+      if (response) {
+        return new Response(response.body, {
+          headers: {
+            "Cache-Control":
+              "public, s-maxage=604800, stale-while-revalidate=86400",
+          },
+        });
       }
     }
-  } catch (e) {
-    return NextResponse.redirect(avatarURL);
   }
+
   return respondWithSVG(name, 240);
 }
 
