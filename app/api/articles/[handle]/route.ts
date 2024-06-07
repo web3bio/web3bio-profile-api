7aae9b1553e98102d828247c69f1ffc5cfc40618
billy <Billy@mask.io>
2024-06-07 17:33:02 +0800

    chore: update

--- app/api/articles/[handle]/route.ts
+++ app/api/articles/[handle]/route.ts
@@ -17,11 +17,6 @@ enum ArticlePlatform {
   mirror = "mirror",
 }
 
-const emptyReturn = () =>
-  NextResponse.json({
-    items: [],
-  });
-
 const fetchRss = async (handle: string, limit: number) => {
   return await fetch(
     `https://contenthash.web3.bio/api/rss?query=${handle}&mode=list&limit=${limit}`
@@ -41,12 +36,7 @@ const fetchArticle = async (
     body: JSON.stringify({
       addresses: [address],
       limit,
-      platform:
-        platform === ArticlePlatform.mirror
-          ? 1
-          : platform === ArticlePlatform.paragraph
-          ? 2
-          : 0,
+      platform: platform === ArticlePlatform.contenthash ? "" : platform,
     }),
   }).then((res) => res.json());
 };
@@ -57,75 +47,93 @@ export async function GET(req: NextRequest) {
   const limit = parseInt(searchParams.get("limit") || "") || 10;
   const platform = searchParams.get("platform") || "";
   const system = handleSearchPlatform(handle);
-  let result = {} as any;
+  const emptyResultStruct = {
+    title: "",
+    link: "",
+    description: "",
+    image: "",
+    items: [] as any,
+  };
+  const emptyReturn = () =>
+    NextResponse.json(JSON.stringify(emptyResultStruct));
+  let result = { ...emptyResultStruct };
   let rssArticles = {} as any;
+
   const profile = await fetch(
     baseURL + `/api/profile/${system}/${handle}`
   ).then((res) => res.json());
+
   if (!profile) return emptyReturn();
+
   if (
     [PlatformType.ethereum, PlatformType.ens, PlatformType.dotbit].includes(
       system
+    ) &&
+    ![ArticlePlatform.mirror, ArticlePlatform.paragraph].includes(
+      platform as ArticlePlatform
     )
   ) {
     rssArticles = await fetchRss(profile.identity, limit);
-    result = {
-      title: rssArticles.title,
-      link: rssArticles.link,
-      description: rssArticles.description,
-      image: rssArticles.image || null,
-      items: [
-        ...rssArticles.items?.map((x: any) => ({
-          ...x,
-          platform: ArticlePlatform.contenthash,
-          body: x.description,
-          published: new Date(x.published).getTime(),
-        })),
-      ],
-    };
+    if (rssArticles?.items)
+      result = {
+        title: rssArticles.title,
+        link: rssArticles.link,
+        description: rssArticles.description,
+        image: rssArticles.image || null,
+        items: [
+          ...rssArticles.items?.map((x: any) => ({
+            ...x,
+            platform: ArticlePlatform.contenthash,
+            body: x.description,
+            published: new Date(x.published).getTime(),
+          })),
+        ],
+      };
   }
 
-  const fireflyArticles = await fetchArticle(
-    profile.address,
-    limit,
-    platform as ArticlePlatform
-  );
-  fireflyArticles?.data.map((x: any) => {
-    const content = JSON.parse(x.content_body);
-    if (x.platform === 1) {
-      // mirror
-      result.items.push({
-        title: content.content.title,
-        link: `${MirrorBaseURL}/${profile.identity}/${x.original_id}`,
-        author: "",
-        thumbnail: "",
-        description: subStr(content.content.body),
-        published: x.content_timestamp * 1000,
-        body: content.content.body,
-        platform: ArticlePlatform.mirror,
-      });
-    } else {
-      // paragraph
-      result.items.push({
-        title: content.title,
-        link: content.url
-          ? `https://${content.url}`
-          : `${ParagraphBaseURL}/@${profile.identity}/${content.slug}`,
-        author: "",
-        thumbnail: "",
-        description: subStr(content.markdown),
-        published: x.content_timestamp * 1000,
-        body: content.markdown,
-        platform: ArticlePlatform.paragraph,
-      });
-    }
-  });
-  result.items = [
-    ...result.items
-      .sort((a: any, b: any) => {
-        return b.published - a.published;
-      })
-      .slice(0, limit),
-  ];
+  if (platform !== ArticlePlatform.contenthash) {
+    const fireflyArticles = await fetchArticle(
+      profile.address,
+      limit,
+      platform as ArticlePlatform
+    );
+    fireflyArticles?.data?.map((x: any) => {
+      const content = JSON.parse(x.content_body);
+      if (x.platform === 1) {
+        // mirror
+        result.items.push({
+          title: content.content.title,
+          link: `${MirrorBaseURL}/${profile.identity}/${x.original_id}`,
+          author: "",
+          thumbnail: "",
+          description: subStr(content.content.body),
+          published: x.content_timestamp * 1000,
+          body: content.content.body,
+          platform: ArticlePlatform.mirror,
+        });
+      } else {
+        // paragraph
+        result.items.push({
+          title: content.title,
+          link: content.url
+            ? `https://${content.url}`
+            : `${ParagraphBaseURL}/@${profile.identity}/${content.slug}`,
+          author: "",
+          thumbnail: "",
+          description: subStr(content.markdown),
+          published: x.content_timestamp * 1000,
+          body: content.markdown,
+          platform: ArticlePlatform.paragraph,
+        });
+      }
+    });
+    result.items = [
+      ...result.items
+        .sort((a: any, b: any) => {
+          return b.published - a.published;
+        })
+        .slice(0, limit),
+    ];
+  }
   return respondWithCache(JSON.stringify(result));
 }
