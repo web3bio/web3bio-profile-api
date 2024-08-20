888ef3a3f46aa1a4ae60c97f635bc6faaa4d8cea
billy <Billy@mask.io>
2024-08-20 17:45:44 +0800

    chore: kill article

--- utils/types.ts
+++ utils/types.ts
@@ -87,24 +87,3 @@ export interface IdentityRecord {
   reverse: boolean;
   expiredAt: string;
 }
-
-export interface ArticleResponse {
-  sites: ArticleSite[]
-  items: ArticleItem[];
-}
-export interface ArticleItem {
-  title: string;
-  description: string;
-  link: string;
-  published: number;
-  platform: string;
-  body: string;
-}
-
-export interface ArticleSite{
-  platform:string,
-  name:string,
-  description: string,
-  image:string,
-  link:string,
-}
