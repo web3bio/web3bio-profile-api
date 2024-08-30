6ac6fd39decb644709525ae4b6dc9567e17aba69
Yan <picturepan2@hotmail.com>
2024-08-30 19:03:49 +0800

    Update test-utils.ts

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -1,7 +1,7 @@
-import { baseURL } from "./base";
+import { BASE_URL } from "./base";
 
 // const baseURL = 'https://api.web3.bio'
 
 export const queryClient = async (path: string) => {
-  return await fetch(baseURL + path);
+  return await fetch(BASE_URL + path);
 };
