28503cbc417bcc8bd44e88069671e70e597f5fa5
Yan <picturepan2@hotmail.com>
2024-10-06 21:14:14 +0700

    Upgrade Next

--- app/api/ns/[handle]/route.ts
+++ app/api/ns/[handle]/route.ts
@@ -6,10 +6,14 @@ import {
 } from "@/utils/base";
 import { NextRequest } from "next/server";
 import { resolveUniversalHandle } from "../../profile/[handle]/utils";
+import { regexSolana, regexBtc } from "@/utils/regexp";
 
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
-  const inputName = searchParams.get("handle")?.toLowerCase() || "";
+  const handle = searchParams.get("handle") || "";
+  const inputName = [regexSolana, regexBtc].some((x) => x.test(handle))
+    ? handle
+    : handle.toLowerCase();
   const platform = handleSearchPlatform(inputName);
   if (!inputName || !platform || !shouldPlatformFetch(platform)) {
     return errorHandle({
