28615d152691f84a0c738ed7cf6963d4ca5ad205
billy <Billy@mask.io>
2024-11-20 18:26:14 +0800

    chore: batch ns get

--- app/api/ns/batch/route.ts
+++ app/api/ns/batch/route.ts
@@ -1,21 +1,42 @@
 import { respondWithCache } from "@/utils/base";
 import { NextRequest, NextResponse } from "next/server";
-import { fetchIdentityGraphBatch } from "../../profile/batch/utils";
 import { PlatformType } from "@/utils/platform";
+import { fetchIdentityGraphBatch } from "../../profile/batch/utils";
+
+const filterIds = (ids: string[]) =>
+  ids.filter((x: string) => {
+    return [
+      PlatformType.ens,
+      PlatformType.basenames,
+      PlatformType.ethereum,
+      PlatformType.lens,
+      PlatformType.farcaster,
+    ].includes(x.split(",")[0] as PlatformType);
+  });
 
 export async function POST(req: NextRequest) {
   const { ids } = await req.json();
   if (!ids.length) return NextResponse.json([]);
   try {
-    const queryIds = ids.filter((x: string) => {
-      return [
-        PlatformType.ens,
-        PlatformType.basenames,
-        PlatformType.ethereum,
-        PlatformType.lens,
-        PlatformType.farcaster,
-      ].includes(x.split(",")[0] as PlatformType);
+    const queryIds = filterIds(ids);
+    const json = await fetchIdentityGraphBatch(queryIds, true);
+    return respondWithCache(JSON.stringify(json));
+  } catch (e: any) {
+    return NextResponse.json({
+      error: e.message,
     });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  try {
+    const ids = searchParams.get("ids")?.split(",") || [];
+    const mergedIds = [];
+    for (let i = 0; i < ids.length; i += 2) {
+      mergedIds.push(`${ids[i]},${ids[i + 1]}`);
+    }
+    const queryIds = filterIds(mergedIds);
     const json = await fetchIdentityGraphBatch(queryIds, true);
     return respondWithCache(JSON.stringify(json));
   } catch (e: any) {
