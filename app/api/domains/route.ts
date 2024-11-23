7383a9a2292d80d2143a88f176666c704eb6a282
billy <Billy@mask.io>
2024-11-23 16:37:57 +0800

    chore: avaialbales proxy

---
+++ app/api/domains/route.ts
@@ -0,0 +1,73 @@
+import { getUserHeaders, respondWithCache } from "@/utils/base";
+import { AuthHeaders, ErrorMessages } from "@/utils/types";
+import { NextRequest, NextResponse } from "next/server";
+import { NEXTID_GRAPHQL_ENDPOINT } from "../profile/[handle]/utils";
+
+const GET_AVAILABLE_DOMAINS = `
+  query GET_AVAILABLE_DOMAINS($name: String!) {
+    domainAvailableSearch(name: $name) {
+      platform
+      name
+      expiredAt
+      availability
+      status
+    }
+  }
+`;
+
+const queryDomains = async (handle: string, headers: AuthHeaders) => {
+  try {
+    const response = await fetch(NEXTID_GRAPHQL_ENDPOINT, {
+      method: "POST",
+      headers: {
+        ...headers,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: GET_AVAILABLE_DOMAINS,
+        variables: {
+          name: handle,
+        },
+      }),
+    });
+
+    return await response.json();
+  } catch (e) {
+    return {
+      errors: e,
+    };
+  }
+};
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const headers = getUserHeaders(req);
+  try {
+    const json = await queryDomains(body.name, headers);
+    return respondWithCache(JSON.stringify(json));
+  } catch (e) {
+    return NextResponse.json({
+      error: e,
+    });
+  }
+}
+
+export async function GET(req: NextRequest) {
+  const { searchParams } = req.nextUrl;
+  const name = searchParams.get("name");
+  const headers = getUserHeaders(req);
+  if (!name)
+    return NextResponse.json({
+      code: 404,
+      message: ErrorMessages.invalidIdentity,
+    });
+  try {
+    const json = await queryDomains(name, headers);
+
+    return respondWithCache(JSON.stringify(json));
+  } catch (e) {
+    return NextResponse.json({
+      error: e,
+    });
+  }
+}
