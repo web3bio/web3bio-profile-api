f88ebb57acc87b8c838d511b42023c8e223e8364
billy <Billy@mask.io>
2025-02-27 10:42:48 +0800

    chore: updates

--- __tests__/universal.test.js
+++ __tests__/universal.test.js
@@ -8,6 +8,7 @@ describe("Test For Universal Profile API", () => {
     expect(res.status).toBe(200);
     const json = await res.json();
     expect(json[0].address).toBe("0x7cbba07e31dc7b12bb69a1209c5b11a8ac50acf5");
+    expect(json[0].displayName).toBe("0x7cbb...acf5");
     expect(json[1].platform).toBe("farcaster");
   });
   it("It should response 200 data for lilgho.lens", async () => {
--- utils/query.ts
+++ utils/query.ts
@@ -170,9 +170,14 @@ export const primaryDomainResolvedRequestArray = (
     platform: recordPlatform,
     resolvedAddress,
     identityGraph,
+    profile,
   } = resolvedRecord;
-  const defaultReturn = resolvedRecord.profile
-    ? { ...resolvedRecord.profile, isPrimary: resolvedRecord.isPrimary }
+  const defaultReturn = profile
+    ? {
+        ...profile,
+        isPrimary: resolvedRecord.isPrimary,
+        displayName: profile.displayName || formatText(identity),
+      }
     : {
         address: resolvedAddress?.[0]?.address || null,
         identity,
@@ -245,7 +250,6 @@ export const primaryDomainResolvedRequestArray = (
           }
         })
         .map((x) => ({ ...x.profile, isPrimary: x.isPrimary })) || [];
-
     if (
       (recordPlatform === PlatformType.ethereum &&
         !vertices.some((x) =>
