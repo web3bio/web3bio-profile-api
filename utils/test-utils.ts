3f978f67f4251c1db43491f6533201bbfb369888
billy <Billy@mask.io>
2025-04-02 17:30:32 +0800

    chore: test for failure

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -1,6 +1,5 @@
 export const queryClient = async (path: string, options?: any) => {
   const baseUrl = process.env.BASE_URL || "http://localhost:3000";
-  console.log(baseUrl, "test url");
   return await fetch(baseUrl + path, {
     headers: {
       "x-api-key": process.env.GENERAL_IDENTITY_GRAPH_API_KEY || "",
