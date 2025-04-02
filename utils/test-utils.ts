c4da153bce3ad7ea230603290668639d9da2035d
billy <Billy@mask.io>
2025-04-02 16:08:00 +0800

    chore: test after vercel build

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -2,7 +2,6 @@ export const queryClient = async (path: string, options?: any) => {
   const baseUrl = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:3000";
-  console.log(baseUrl, "url");
   return await fetch(baseUrl + path, {
     headers: {
       "x-api-key": process.env.GENERAL_IDENTITY_GRAPH_API_KEY || "",
