5c8697051b7436ed802fdb8f94b2232cdd5e3af9
billy <Billy@mask.io>
2025-04-02 16:02:27 +0800

    chore: updates

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -2,5 +2,11 @@ export const queryClient = async (path: string, options?: any) => {
   const baseUrl = process.env.VERCEL_URL
     ? `https://${process.env.VERCEL_URL}`
     : "http://localhost:3000";
-  return await fetch(baseUrl + path, { ...options });
+  console.log(baseUrl, "url");
+  return await fetch(baseUrl + path, {
+    headers: {
+      "x-api-key": process.env.GENERAL_IDENTITY_GRAPH_API_KEY || "",
+    },
+    ...options,
+  });
 };
