e752b3cb969fe5ad7ccb0ddc5f50e92baa3373ab
billy <Billy@mask.io>
2025-05-28 18:50:38 +0800

    chore: updates

--- utils/networks.ts
+++ utils/networks.ts
@@ -1,23 +1,10 @@
-import { type Network, NETWORK_DATA } from "web3bio-profile-kit";
+import { NETWORK_DATA } from "web3bio-profile-kit";
 
 export const networkByIdOrName = (id: number, name?: string) => {
   return Object.values(NETWORK_DATA).find((x) => {
     if (x.chainId === id) return x;
     if (name && [x.key, x.short].includes(name)) return x;
   });
-};
-
-export const NetworkMapping = (network: Network) => {
-  return (
-    NETWORK_DATA[network] ?? {
-      key: network,
-      icon: "",
-      label: network,
-      primaryColor: "#000000",
-      bgColor: "#efefef",
-      scanPrefix: "",
-    }
-  );
 };
 
 export const chainIdToNetwork = (chainId?: number, useShort?: boolean) => {
--- utils/source.ts
+++
@@ -1,5 +0,0 @@
-// sources.ts
-import { SOURCE_DATA, SourceInfo, SourceType } from "web3bio-profile-kit";
-
-export const getSourceInfo = (sourceKey: SourceType): SourceInfo =>
-  SOURCE_DATA[sourceKey] || { name: sourceKey, description: "Unknown source" };
