6e6ba78ed68ffb8af3a58961a30f43d44365555a
billy <Billy@mask.io>
2025-10-14 15:50:44 +0800

    chore: fix

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -1,10 +1,8 @@
 export const queryClient = async (path: string) => {
-  const baseUrl = "http://localhost:8787";
-  // const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+  const baseUrl = process.env.BASE_URL || "http://localhost:3000";
   return await fetch(baseUrl + path, {
     headers: {
-      "x-api-key":
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiMzE0NjYzIiwiZXhwIjoyMjA1OTExNjE0LCJyb2xlIjo2fQ.hK5V1rVNJ-z5p5JlEeKMmzhLOZxGhqCyVLwQm-3iRPQ",
+      "x-api-key": process.env.GENERAL_IDENTITY_GRAPH_API_KEY || "",
     },
   });
 };
