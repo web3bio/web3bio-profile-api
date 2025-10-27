ea77b917a69afe437e0e54ce97132faa21b23c89
billy <Billy@mask.io>
2025-10-27 16:28:34 +0800

    chore: test

--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -1,5 +1,6 @@
 export const queryClient = async (path: string) => {
   const baseUrl = process.env.BASE_URL || "http://localhost:3000";
+  console.log(baseUrl, "base");
   return await fetch(baseUrl + path, {
     headers: {
       "x-api-key": process.env.GENERAL_IDENTITY_GRAPH_API_KEY || "",
