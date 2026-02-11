3b870e805c571c8864375852496be09bccc7a779
billy <Billy@mask.io>
2026-02-11 13:09:21 +0800

    chore: updates

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,6 @@ import type { Config } from "jest";
 import nextJest from "next/jest.js";
 
 const createJestConfig = nextJest({
-  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
   dir: "./",
 });
 
@@ -11,11 +10,11 @@ const config: Config = {
   coverageProvider: "v8",
   testEnvironment: "node",
   testTimeout: 200000,
+  setupFiles: ["<rootDir>/utils/setup.ts"],
   transform: {
     "^.+\\.tsx?$": "ts-jest",
   },
   transformIgnorePatterns: ["/node_modules/"],
 };
 
-// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
 export default createJestConfig(config);
--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -1,17 +1,3 @@
-if (process.env.NODE_ENV !== "production") {
-  try {
-    require("fs")
-      .readFileSync(".env.local", "utf8")
-      .split("\n")
-      .forEach((line: string) => {
-        const [key, value] = line.split("=");
-        if (key && value && !process.env[key.trim()]) {
-          process.env[key.trim()] = value.trim().replace(/^["']|["']$/g, "");
-        }
-      });
-  } catch (e) {}
-}
-
 export const queryClient = async (path: string) => {
   const baseUrl = process.env.BASE_URL || "http://localhost:3000";
   const API_KEY = process.env.GENERAL_IDENTITY_GRAPH_API_KEY || "";
