fd2dd8c961eaf9b6d758a2bb24f49dde2e84ad29
billy <Billy@mask.io>
2026-03-24 14:32:00 +0800

    choreL updates

---
+++ __tests__/profile/web2.test.js
@@ -0,0 +1,19 @@
+import { queryClient } from "../../utils/test-utils";
+
+describe("Test For Profile Web2 API", () => {
+  it("It should response 403 for invalid role", async () => {
+    const res = await queryClient("/profile/sujiyan.eth/web2", {
+      "x-api-key": "123456",
+    });
+    console.log(res,'kk')
+    expect(res.status).toBe(403);
+  });
+  it("It should response 200 for sujiyan.eth", async () => {
+    const res = await queryClient("/profile/sujiyan.eth/web2");
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(json.some((x) => x.platform === "instagram")).toBe(true);
+    expect(json.some((x) => x.platform === "reddit")).toBe(true);
+    expect(json.some((x) => x.platform === "github")).toBe(true);
+  });
+});
--- utils/test-utils.ts
+++ utils/test-utils.ts
@@ -2,11 +2,11 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env.local" });
 
-export const queryClient = async (path: string) => {
+export const queryClient = async (path: string, headers?: any) => {
   const baseUrl = process.env.BASE_URL || "http://localhost:3000";
   const API_KEY = process.env.GENERAL_IDENTITY_GRAPH_API_KEY || "";
 
   return fetch(baseUrl + path, {
-    headers: { "x-api-key": API_KEY },
+    headers: { "x-api-key": API_KEY, ...headers },
   });
 };
