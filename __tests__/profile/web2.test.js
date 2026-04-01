7daf985c3bb4b70c0c448a84557e375b398f1ab0
billy <Billy@mask.io>
2026-04-01 14:16:21 +0800

    chore: updates

--- __tests__/profile/web2.test.js
+++ __tests__/profile/web2.test.js
@@ -12,4 +12,15 @@ describe("Test For Profile Web2 API", () => {
       json.find((x) => x.platform === "instagram").links.website.handle,
     ).toBe("dimension.im");
   });
+  it("It should response 200 for accountless.eth", async () => {
+    const res = await queryClient("/profile/accountless.eth/web2");
+    expect(res.status).toBe(200);
+    const json = await res.json();
+    expect(
+      json.find((x) => x.platform === "ens").links.website.handle,
+    ).toBe("linktr.ee/alexanderchopan");
+    expect(
+      json.find((x) => x.platform === "github").links.website.handle,
+    ).toBe("linktr.ee/alexanderchopan");
+  });
 });
