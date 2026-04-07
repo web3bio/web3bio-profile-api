6759d6a1cee506efc2336f4b5e5a118af8d642cc
billy <Billy@mask.io>
2026-04-07 12:02:56 +0800

    chore: updates

--- __tests__/profile/web2.test.js
+++ __tests__/profile/web2.test.js
@@ -2,7 +2,7 @@ import { queryClient } from "../../utils/test-utils";
 
 describe("Test For Profile Web2 API", () => {
   it("It should response 200 for sujiyan.eth", async () => {
-    const res = await queryClient("/profile/sujiyan.eth/web2");
+    const res = await queryClient("/profile/web2/sujiyan.eth");
     expect(res.status).toBe(200);
     const json = await res.json();
     expect(json.some((x) => x.platform === "instagram")).toBe(true);
@@ -13,7 +13,7 @@ describe("Test For Profile Web2 API", () => {
     ).toBe("dimension.im");
   });
   it("It should response 200 for accountless.eth", async () => {
-    const res = await queryClient("/profile/accountless.eth/web2");
+    const res = await queryClient("/profile/web2/accountless.eth");
     expect(res.status).toBe(200);
     const json = await res.json();
     expect(
